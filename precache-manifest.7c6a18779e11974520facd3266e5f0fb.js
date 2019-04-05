010c73e4cb96d2516399f4a9e5f427b0d8cd9a06
Aqarius90 <Aqarius90@gmail.com>
2019-04-06 00:39:41 +0200

    Updates

---
+++ precache-manifest.7c6a18779e11974520facd3266e5f0fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c01046afc4aed0d75056",
+    "url": "/SteelDivisionDB/static/js/runtime~main.69ec7a86.js"
+  },
+  {
+    "revision": "e180b8e111448f7881dd",
+    "url": "/SteelDivisionDB/static/js/main.a4f20076.chunk.js"
+  },
+  {
+    "revision": "f9d06c8946fada79630b",
+    "url": "/SteelDivisionDB/static/js/2.67689d47.chunk.js"
+  },
+  {
+    "revision": "e180b8e111448f7881dd",
+    "url": "/SteelDivisionDB/static/css/main.34cab8c1.chunk.css"
+  },
+  {
+    "revision": "f9d06c8946fada79630b",
+    "url": "/SteelDivisionDB/static/css/2.b10c9cbe.chunk.css"
+  },
+  {
+    "revision": "eb5479f30ec051ad4a258cbb32ccdc4a",
+    "url": "/SteelDivisionDB/index.html"
+  }
+];
